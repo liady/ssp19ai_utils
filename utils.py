b8616d8672458817dae1517e5548ee4ef01d95c1
Liad Yosef <liady@users.noreply.github.com>
2019-07-15 01:13:48 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -99,3 +99,22 @@ def plot_confusion_matrix(y_true, y_pred, classes,
                     color="white" if cm[i, j] > thresh else "black")
     fig.tight_layout()
     return ax
+
+def plot_accuracy_and_loss(history):
+  # Plot training & validation accuracy values
+  plt.plot(history.history['accuracy'])
+  plt.plot(history.history['val_accuracy'])
+  plt.title('Model accuracy')
+  plt.ylabel('Accuracy')
+  plt.xlabel('Epoch')
+  plt.legend(['Train', 'Validation'], loc='upper left')
+  plt.show()
+
+  # Plot training & validation loss values
+  plt.plot(history.history['loss'])
+  plt.plot(history.history['val_loss'])
+  plt.title('Model loss')
+  plt.ylabel('Loss')
+  plt.xlabel('Epoch')
+  plt.legend(['Train', 'Validation'], loc='upper left')
+  plt.show()
