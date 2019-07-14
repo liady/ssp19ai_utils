c823f5d2e7670fd8090b10041aafdb9357749a1b
Liad Yosef <liady@users.noreply.github.com>
2019-07-15 01:05:09 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -17,3 +17,85 @@ def generateDataPoints(number_of_classes, points_per_class):
     X[ix] = np.c_[r*np.sin(t), r*np.cos(t)]
     y[ix] = j
   return X, y
+
+def plot_decision_boundary(X, y, model, steps=100, cmap='Paired'):
+    """
+    Function to plot the decision boundary and data points of a model.
+    Data points are colored based on their actual label.
+    """
+
+    # Define region of interest by data limits
+    x_span = np.linspace(-1, 1, steps)
+    y_span = np.linspace(-1, 1, steps)
+    xx, yy = np.meshgrid(x_span, y_span)
+
+    # Make predictions across region of interest
+    labels = np.argmax(model.predict(np.c_[xx.ravel(), yy.ravel()]), axis=1)
+
+    # Plot decision boundary in region of interest
+    z = labels.reshape(xx.shape)
+
+    fig, ax = plt.subplots()
+    ax.contourf(xx, yy, z, cmap=plt.cm.Spectral, alpha=0.25)
+
+    # Get predicted labels on training data and plot
+    train_labels = model.predict(X)
+    ax.scatter(X[:,0], X[:,1], c=y, cmap=plt.cm.Spectral, lw=0)
+
+    return fig, ax
+  
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Confusion matrix'
+        else:
+            title = 'Confusion matrix'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    unique = unique_labels(y_true, y_pred)
+    classes=classes[unique]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Confusion matrix")
+    else:
+        print('Confusion matrix')
+
+    #print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
